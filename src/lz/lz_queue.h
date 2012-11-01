050d711b43b561e4a705ad4ec36330bfe35ffb2a
GuglielmoS <guglielmo@fachini.net>
2012-11-01 01:34:30 +0100

    added an implementation of a queue

---
+++ src/lz/lz_queue.h
@@ -0,0 +1,27 @@
+#ifndef __LZ_QUEUE__
+#define __LZ_QUEUE__
+
+#include <stdbool.h>
+
+#include "lz_element.h"
+
+typedef LZ_Element* LZ_Queue_Value;
+
+typedef struct __lz_queue_node {
+    LZ_Queue_Value          value; // the value of the node
+    struct __lz_queue_node *next;  // the pointer to the next node
+} LZ_Queue_Node;
+
+typedef struct {
+    LZ_Queue_Node *head, *tail;    // head and tail pointer of the queue
+} LZ_Queue;
+
+/** LZ_Queue accessors **/
+void LZ_Queue_enqueue(LZ_Queue *q, LZ_Queue_Value value);
+LZ_Queue_Value LZ_Queue_dequeue(LZ_Queue *q);
+
+/** Helpers **/
+void LZ_Queue_init(LZ_Queue *q);
+bool LZ_Queue_is_empty(LZ_Queue *q);
+
+#endif /* __LZ_QUEUE__ */
