1f867359b88b6cf32725c81495ead2a2556b1ded
GuglielmoS <guglielmo@fachini.net>
2013-01-17 18:22:39 +0100

    fixed a bug in the output file name generation

--- src/main.c
+++ src/main.c
@@ -16,15 +16,25 @@ void die_exit(const char *exec_file_name)
 
 int main(int argc, char *argv[])
 {
-    Deflate_Params params; // fast = true
+    // default extension of the output compressed file
+    static const char *DF_FILE_EXT = ".dzip";
+
+    // used for creating the output file name in case of compression
+    char *out_fname = NULL;
+
+    // compression/decompression parameters
+    Deflate_Params params;
     bool compress = true;
 
+    // sets the default parameter values
     params.fast = false;
     params.in_file_name = NULL;
     params.out_file_name = NULL;
 
+    // goes through the user parameter and sets the relative
+    // preferences in the data structure
     int c;
-    while ((c = getopt (argc, argv, "fcdi:o:")) != -1) {
+    while ((c = getopt(argc, argv, "fcdi:o:")) != -1) {
         switch (c) {
             case 'c':
                 compress = true;
@@ -46,20 +56,41 @@ int main(int argc, char *argv[])
         }
     }
 
+    // if the user doesn't provide the input file
     if (params.in_file_name == NULL) {
         die_exit(argv[0]);
     }
 
+    // if the output file name isn't provided by the user,
+    // we assume that it will be the input file name with the
+    // extension defined in 'DF_FILE_EXT'.
     if (params.out_file_name == NULL) {
-        params.out_file_name = strcat(strdup(params.in_file_name), ".dzip");
+        size_t in_len = strlen(params.in_file_name),
+               ext_len = strlen(DF_FILE_EXT);
+
+        out_fname = (char*)malloc(in_len + ext_len + 1);
+        for (int i = 0; i < in_len; i++) {
+            out_fname[i] = params.in_file_name[i];
+        }
+        for (int i = 0; i < ext_len; i++) {
+            out_fname[in_len + i] = DF_FILE_EXT[i];
+        }
+        out_fname[in_len + ext_len] = 0x00;
+
+        params.out_file_name = out_fname;
     }
 
+    // compression/decompression routines call
     if (compress) {
         Deflate_encode(&params);
     }
     else {
         Deflate_decode(&params);
     }
+
+    // realese the memory
+    if (out_fname)
+        free(out_fname);
 
     return 0;
 }
